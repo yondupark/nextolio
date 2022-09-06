45f478e1018d9d2b30b27c53a360706830b4a3e6
SangWanPark <parksw8218@gmail.com>
2022-09-06 11:14:26 +0900

    BoilerPlate 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../assets/scss/base/reset.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Nextolio</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Next.js를 활용한 뭔가 만드는 페이지" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
