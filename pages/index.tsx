b11c6e4758fd177fe8816e0efb4d84c28d26726b
SangWanPark <parksw8218@gmail.com>
2022-09-06 13:14:39 +0900

    vibrate test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next';
 
+function vibrate() {
+    if (!window) {
+        return;
+    }
+
+    if (!window.navigator) {
+        return;
+    }
+
+    if (!window.navigator.vibrate) {
+        return;
+    }
+
+    window.navigator.vibrate([200, 100, 200]);
+}
 const Home: NextPage = () => {
     return (
         <>
-            <div className='mainText'>
+            <div className='mainText' onClick={vibrate}>
               BoilerPlate로 사용하자 !
             </div>
         </>
